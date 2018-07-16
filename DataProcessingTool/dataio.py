26d601a267bc4c13b7fff9cc484d3787e0e37a47
Chen-Z-H <31105348+Chen-Z-H@users.noreply.github.com>
2018-07-16 12:23:47 +0800

    Add files via upload

--- DataProcessingTool/dataio.py
+++ DataProcessingTool/dataio.py
@@ -1,17 +1,55 @@
 import numpy as np
 import pandas as pd
+from shapely.geometry import Point
+from shapely.geometry import Polygon
 
 
 points_inside_path = "points_inside.csv"
 points_outside_path = "points_outside.csv"
 
 
-def loadCsvFile(path):
+def loadDataFile(path):
+    data = pd.read_csv(path)
     return None
 
-
-def saveCsvFile(category):
+def loadCoorFile(path):
+    coor = pd.read_csv(path)
     return None
+
+def saveCsvFile(fullpath):
+    return None
+
+def contain():
+    return None
+
+def classify(datapath, coorpath, category, mode):
+    inside_index_list = []          # coors of points inside the given area
+    outside_index_list = []         # coors of points outside the given area
+
+    coor = pd.read_csv(coorpath)
+    num_coor = coor.count()
+    coor_list = None * num_coor
+
+    # build the Polygon
+    for i in range(num_coor):
+        coor_list[i] = (coor.loc[i, ['lon']], coor.loc[i, ['lat']])
+    area = Polygon(coor_list)
+
+    data = pd.read_csv(datapath)
+    num_data_item = data.count()
+
+    for i in range(num_data_item):
+        if (area.contains(Point(data.loc[i, ['lon']], data.loc[i, ['lat']]))):
+            inside_index_list.append(i)
+        else:
+            outside_index_list.append(i)
+
+    if (mode == 0):
+        pass
+    elif (mode == 1):
+        pass
+    else:
+        pass
 
 
 
--- DataProcessingTool/tool.py
+++ DataProcessingTool/tool.py
@@ -1,25 +1,27 @@
 from datetime import datetime
 from PyQt5 import QtCore, QtGui, QtWidgets
 
-class Ui_MainWindow(object):
+class Ui_MainWindow(QtWidgets.QWidget):
 
 
     def __init__(self):
+        super().__init__()
         self.saveMode = 0   # 0 => saving inside points, 1 => saving outside points, 2 => saving both inside and outside
         self.sourceFile = ""
-        self.targetFile = ""
+        self.targetDirectory = ""
+        self.sourceCoorFile = ""
 
     def iniWindow(self, MainWindow):
         self.setupUi(MainWindow)
-        self.loadSourcePushButton.click.connect(self.onSetSourcePathButtonClick)
-        self.loadTargetPushButton.click.connect(self.onSetTargetCategoryButtonClick)
-        self.startPushButton.click.connect(self.onStartButtonClick)
+        self.loadSourcePushButton.clicked.connect(self.onSetSourcePathButtonClick)
+        self.loadTargetPushButton.clicked.connect(self.onSetTargetCategoryButtonClick)
+        self.startPushButton.clicked.connect(self.onStartButtonClick)
 
     def setupUi(self, MainWindow):
         MainWindow.setObjectName("MainWindow")
-        MainWindow.resize(1032, 550)
-        MainWindow.setMinimumSize(QtCore.QSize(1032, 550))
-        MainWindow.setMaximumSize(QtCore.QSize(1032, 550))
+        MainWindow.resize(1025, 526)
+        MainWindow.setMinimumSize(QtCore.QSize(0, 0))
+        MainWindow.setMaximumSize(QtCore.QSize(7000000, 5500000))
         self.centralwidget = QtWidgets.QWidget(MainWindow)
         self.centralwidget.setObjectName("centralwidget")
         self.sourceGroupBox = QtWidgets.QGroupBox(self.centralwidget)
@@ -34,7 +36,7 @@ class Ui_MainWindow(object):
         self.loadSourcePushButton.setCursor(QtGui.QCursor(QtCore.Qt.PointingHandCursor))
         self.loadSourcePushButton.setObjectName("loadSourcePushButton")
         self.targetGroupBox = QtWidgets.QGroupBox(self.centralwidget)
-        self.targetGroupBox.setGeometry(QtCore.QRect(10, 129, 491, 91))
+        self.targetGroupBox.setGeometry(QtCore.QRect(10, 240, 491, 91))
         self.targetGroupBox.setStyleSheet("font: 75 10pt \"楷体\";")
         self.targetGroupBox.setObjectName("targetGroupBox")
         self.targetLineEdit = QtWidgets.QLineEdit(self.targetGroupBox)
@@ -44,27 +46,40 @@ class Ui_MainWindow(object):
         self.loadTargetPushButton.setGeometry(QtCore.QRect(430, 30, 41, 41))
         self.loadTargetPushButton.setCursor(QtGui.QCursor(QtCore.Qt.PointingHandCursor))
         self.loadTargetPushButton.setObjectName("loadTargetPushButton")
+        self.targetLineEdit.raise_()
+        self.loadTargetPushButton.raise_()
         self.logGroupBox = QtWidgets.QGroupBox(self.centralwidget)
-        self.logGroupBox.setGeometry(QtCore.QRect(520, 10, 491, 471))
+        self.logGroupBox.setGeometry(QtCore.QRect(520, 10, 491, 451))
         self.logGroupBox.setStyleSheet("font: 75 10pt \"楷体\";")
         self.logGroupBox.setObjectName("logGroupBox")
         self.logTextEdit = QtWidgets.QTextEdit(self.logGroupBox)
-        self.logTextEdit.setGeometry(QtCore.QRect(20, 30, 451, 421))
+        self.logTextEdit.setGeometry(QtCore.QRect(20, 30, 451, 401))
         self.logTextEdit.setObjectName("logTextEdit")
         self.saveProgressBar = QtWidgets.QProgressBar(self.centralwidget)
-        self.saveProgressBar.setGeometry(QtCore.QRect(10, 280, 491, 41))
+        self.saveProgressBar.setGeometry(QtCore.QRect(10, 360, 491, 41))
         self.saveProgressBar.setStyleSheet("font: 75 10pt \"楷体\";")
         self.saveProgressBar.setProperty("value", 24)
         self.saveProgressBar.setAlignment(QtCore.Qt.AlignCenter)
         self.saveProgressBar.setObjectName("saveProgressBar")
         self.startPushButton = QtWidgets.QPushButton(self.centralwidget)
-        self.startPushButton.setGeometry(QtCore.QRect(200, 350, 93, 41))
+        self.startPushButton.setGeometry(QtCore.QRect(200, 420, 93, 41))
         self.startPushButton.setCursor(QtGui.QCursor(QtCore.Qt.PointingHandCursor))
         self.startPushButton.setStyleSheet("font: 75 12pt \"楷体\";")
         self.startPushButton.setObjectName("startPushButton")
+        self.sourceCoorGroupBox = QtWidgets.QGroupBox(self.centralwidget)
+        self.sourceCoorGroupBox.setGeometry(QtCore.QRect(10, 120, 491, 91))
+        self.sourceCoorGroupBox.setStyleSheet("font: 75 10pt \"楷体\";")
+        self.sourceCoorGroupBox.setObjectName("sourceCoorGroupBox")
+        self.sourceCoorLineEdit = QtWidgets.QLineEdit(self.sourceCoorGroupBox)
+        self.sourceCoorLineEdit.setGeometry(QtCore.QRect(20, 30, 401, 41))
+        self.sourceCoorLineEdit.setObjectName("sourceCoorLineEdit")
+        self.loadCoorPushButton = QtWidgets.QPushButton(self.sourceCoorGroupBox)
+        self.loadCoorPushButton.setGeometry(QtCore.QRect(430, 30, 41, 41))
+        self.loadCoorPushButton.setCursor(QtGui.QCursor(QtCore.Qt.PointingHandCursor))
+        self.loadCoorPushButton.setObjectName("loadCoorPushButton")
         MainWindow.setCentralWidget(self.centralwidget)
         self.menubar = QtWidgets.QMenuBar(MainWindow)
-        self.menubar.setGeometry(QtCore.QRect(0, 0, 1032, 26))
+        self.menubar.setGeometry(QtCore.QRect(0, 0, 1025, 26))
         self.menubar.setObjectName("menubar")
         self.programMenu = QtWidgets.QMenu(self.menubar)
         self.programMenu.setObjectName("programMenu")
@@ -76,20 +91,19 @@ class Ui_MainWindow(object):
         MainWindow.setStatusBar(self.statusbar)
         self.exitAction = QtWidgets.QAction(MainWindow)
         self.exitAction.setObjectName("exitAction")
-        self.exitAction.triggered.connect(QtWidgets.QApplication.quit)
         self.insideAreaAction = QtWidgets.QAction(MainWindow)
-        self.insideAreaAction.setObjectName("insideAreaAction")
         self.insideAreaAction.setCheckable(True)
         self.insideAreaAction.setChecked(True)
+        self.insideAreaAction.setObjectName("insideAreaAction")
         self.insideAreaAction.triggered.connect(self.setModeInside)
         self.outsideAreaAction = QtWidgets.QAction(MainWindow)
-        self.outsideAreaAction.setObjectName("outsideAreaAction")
         self.outsideAreaAction.setCheckable(True)
+        self.outsideAreaAction.setObjectName("outsideAreaAction")
         self.outsideAreaAction.triggered.connect(self.setModeOutside)
         self.inoutAction = QtWidgets.QAction(MainWindow)
+        self.inoutAction.setCheckable(True)
         self.inoutAction.setObjectName("inoutAction")
         self.inoutAction.triggered.connect(self.setModeInOut)
-        self.inoutAction.setCheckable(True)
         self.programMenu.addAction(self.exitAction)
         self.optionMenu.addAction(self.insideAreaAction)
         self.optionMenu.addAction(self.outsideAreaAction)
@@ -103,12 +117,14 @@ class Ui_MainWindow(object):
     def retranslateUi(self, MainWindow):
         _translate = QtCore.QCoreApplication.translate
         MainWindow.setWindowTitle(_translate("MainWindow", "MainWindow"))
-        self.sourceGroupBox.setTitle(_translate("MainWindow", "源文件路径"))
+        self.sourceGroupBox.setTitle(_translate("MainWindow", "数据文件路径"))
         self.loadSourcePushButton.setText(_translate("MainWindow", "..."))
         self.targetGroupBox.setTitle(_translate("MainWindow", "输出目录"))
         self.loadTargetPushButton.setText(_translate("MainWindow", "..."))
         self.logGroupBox.setTitle(_translate("MainWindow", "日志"))
         self.startPushButton.setText(_translate("MainWindow", "开始"))
+        self.sourceCoorGroupBox.setTitle(_translate("MainWindow", "坐标文件路径"))
+        self.loadCoorPushButton.setText(_translate("MainWindow", "..."))
         self.programMenu.setTitle(_translate("MainWindow", "程序"))
         self.optionMenu.setTitle(_translate("MainWindow", "选项"))
         self.exitAction.setText(_translate("MainWindow", "退出"))
@@ -138,17 +154,33 @@ class Ui_MainWindow(object):
         self.inoutAction.setChecked(True)
 
     def onSetSourcePathButtonClick(self):
-        fileName, filetype = QtWidgets.QFileDialog.getOpenFileName(self,
-                                                                    "选取源数据文件路径",
-                                                                    "./",
-                                                                    "Csv Files (*.csv)")
+        # print("aaaa")
+        fileName, _ = QtWidgets.QFileDialog.getOpenFileName(self,
+                                                         "选取源数据文件路径",
+                                                         "./",
+                                                         "Csv Files (*.csv)")
         self.sourceLineEdit.setText(fileName)
         sourceFile = fileName
+        self.addLog("当前源文件路径为：" + sourceFile)
         # pass
 
+    def onSetSourceCoorPathButtonClick(self):
+        fileName, _ = QtWidgets.QFileDialog.getOpenFileName(self,
+                                                            "选取源数据文件路径",
+                                                            "./",
+                                                            "Csv Files (*.csv)")
+        self.sourceCoorLineEdit.setText(fileName)
+        sourceCoorFile = fileName
+        self.addLog("当前源文件路径为：" + sourceCoorFile)
+
     def onSetTargetCategoryButtonClick(self):
-        directory1 = QtWidgets.QFileDialog.getExistingDirectory
-        pass
+        directory = QtWidgets.QFileDialog.getExistingDirectory(self,
+                                                               "选取目标路径",
+                                                               "./")
+        self.targetLineEdit.setText(directory)
+        targetDirectory = directory
+        self.addLog("当前坐标文件目录为：" + targetDirectory)
+        # pass
 
     def onStartButtonClick(self):
         pass
